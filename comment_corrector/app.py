2b8f4e09b0bc07c85bd6469574c627136d890ed2
Clodagh Walsh <clodaghwalsh17@gmail.com>
2023-01-02 15:13:53 +0000

    Extract comments from supported file types
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- comment_corrector/app.py
+++ comment_corrector/app.py
@@ -1,2 +1,13 @@
-def run():
-    print("hello world")
+from comment_corrector.extract_comments import list_comments
+import argparse
+
+def init_argparse():        
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help = "File to be parsed")
+    args = parser.parse_args()
+    return args
+
+def run():     
+    args = init_argparse()
+    comments = list_comments(args.file)
+    print(comments)
