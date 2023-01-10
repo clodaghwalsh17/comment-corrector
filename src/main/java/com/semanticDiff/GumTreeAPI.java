2fb0d81b615cfac08744431da287dd706e9fce79
Clodagh Walsh <clodaghwalsh17@gmail.com>
2023-01-10 11:44:36 +0000

    Add support for multiple languages for semantic differencing step
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- src/main/java/com/semanticDiff/GumTreeAPI.java
+++ src/main/java/com/semanticDiff/GumTreeAPI.java
@@ -2,48 +2,44 @@ package com.semanticDiff;
 
 import java.io.IOException;
 
-// import com.github.gumtreediff.client.Run;
-// import com.github.gumtreediff.gen.*;
-import com.github.gumtreediff.gen.jdt.*;
+import com.github.gumtreediff.gen.*;
+import com.github.gumtreediff.io.TreeIoUtils;
 import com.github.gumtreediff.matchers.*;
 import com.github.gumtreediff.actions.*;
 import com.github.gumtreediff.actions.model.*;
 import com.github.gumtreediff.tree.*;
 
-
 public class GumTreeAPI {
-    // public static void main(String[] args) throws UnsupportedOperationException, IOException {
-    //     Run.initGenerators(); // registers the available parsers
-    //     String srcFile = "ExampleA.java";
-    //     String dstFile = "ExampleB.java";
-    //     Tree src = TreeGenerators.getInstance().getTree(srcFile).getRoot(); // retrieves and applies the default parser for the file 
-    //     Tree dst = TreeGenerators.getInstance().getTree(dstFile).getRoot(); // retrieves and applies the default parser for the file 
-    //     Matcher defaultMatcher = Matchers.getInstance().getMatcher(); // retrieves the default matcher
-    //     MappingStore mappings = defaultMatcher.match(src, dst); // computes the mappings between the trees
-    //     EditScriptGenerator editScriptGenerator = new SimplifiedChawatheScriptGenerator(); // instantiates the simplified Chawathe script generator
-    //     EditScript actions = editScriptGenerator.computeActions(mappings); // computes the edit script
-    //     System.out.println(actions);
-    // }
-
     public static void main(String[] args) throws IOException {
-        if(args.length != 2) {
-            System.err.println("Two files must be provided to perform semantic differencing");
+        if(args.length != 3) {
+            System.err.println("Insufficient arguments provided. \nThe programming language of the source files, as well as the file path to two files must be provided to perform semantic differencing");
             System.exit(1);
         } 
 
-        String srcFile = args[0];
-        String dstFile = args[1];
+        String programmingLanguage = args[0];
+        String srcFile = args[1];
+        String dstFile = args[2];
 
-        Tree src = new JdtTreeGenerator().generateFrom().file(srcFile).getRoot();
-        Tree dst = new JdtTreeGenerator().generateFrom().file(dstFile).getRoot();
-        Matcher defaultMatcher = Matchers.getInstance().getMatcher(); // retrieves the default matcher
-        MappingStore mappings = defaultMatcher.match(src, dst); // computes the mappings between the trees
-        EditScriptGenerator editScriptGenerator = new SimplifiedChawatheScriptGenerator(); // instantiates the simplified Chawathe script generator
+        TreeGenerator treeGenerator = TreeGeneratorFactory.treeGenerator(programmingLanguage);
+
+        if(treeGenerator == null) {
+            System.err.println("Unable to find a suitable parser. The programming language specified is currently unsupported.");
+            System.exit(1);
+        }
+
+        Tree src = treeGenerator.generateFrom().file(srcFile).getRoot();
+        Tree dst = treeGenerator.generateFrom().file(dstFile).getRoot();
+        Matcher defaultMatcher = Matchers.getInstance().getMatcher(); 
+        MappingStore mappings = defaultMatcher.match(src, dst); 
+        EditScriptGenerator editScriptGenerator = new SimplifiedChawatheScriptGenerator(); 
         EditScript actions = editScriptGenerator.computeActions(mappings);
         
         for(Action a: actions) {
             System.out.println(a);
         }
-    }
 
+        TreeContext tc = treeGenerator.generateFrom().file(dstFile);
+        System.out.println("\n----------------------- JSON Format of tree -----------------------\n");
+        System.out.println(TreeIoUtils.toJson(tc));
+    }
 }
