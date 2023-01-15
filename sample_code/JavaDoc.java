40d409e3b2c8932e1cf03f66ef39f564e67c0f28
Clodagh Walsh <clodaghwalsh17@gmail.com>
2023-01-15 12:12:39 +0000

    sample_code: Add Javadoc test case
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

---
+++ sample_code/JavaDoc.java
@@ -0,0 +1,11 @@
+public class JavaDoc {
+    /**
+     * Calculate the sum of two integers
+     * @param x First parameter to add method
+     * @param y  Second parameter to add method
+     * @return int The sum is returned.
+     */
+    public int add(int x, int y) {
+        return x+y;
+    } 
+}
