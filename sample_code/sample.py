c46bcbc8b290f56fe01ce6e3d645ed17263b9bca
Clodagh Walsh <clodaghwalsh17@gmail.com>
2023-01-16 18:47:04 +0000

    Add Python test cases
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- sample_code/sample.py
+++ sample_code/sample.py
@@ -1,4 +1,13 @@
-#Comment
-print("Hello World")
+import random
 
-pi = 3.14 # Value of Pi
+PI = 3.14 # Value of pi
+
+# Find area of circle with random radius
+def area():   
+   r = random.randint(1, 5) # Random value between 1 and 5
+   area = PI * r * r
+   # Print information
+   print("Radius={}, Area={}".format(r,area))
+
+print("Pi={}\n".format(PI))
+area()
