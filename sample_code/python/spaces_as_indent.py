b4409e9edb8bb61ae217f94163c590c97045153e
Clodagh Walsh <clodaghwalsh17@gmail.com>
2023-01-17 13:13:04 +0000

    Add test for using tab or spaces for indent and move all Python tests into a folder.
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

---
+++ sample_code/python/spaces_as_indent.py
@@ -0,0 +1,7 @@
+# Test for 4 spaces used as indentation
+
+def string_manipulation(str):
+    # Display original string
+    print(str)
+    str = str.capitalize() # Capitalise first letter of string
+    print(str)
