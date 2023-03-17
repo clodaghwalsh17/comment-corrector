4ae38303b5ce6e790f667bb232c4c556a522a47a
Clodagh Walsh <clodaghwalsh17@gmail.com>
2023-03-17 20:48:10 +0000

    Call comment corrector in the entrypoint script on the temporary files v1.py and v2.py
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh -l
-echo "Action Complete"
-echo "::error ::Message from Comment Corrector"
-exit 1
+
+output=$(python3 -m comment_corrector v1.py v2.py)
+
+echo $output
+if [ "$output" = "" ]; then
+    echo "No errors found"
+    exit 0
+else
+    echo "::warning ::Comment Corrector marked the following comments as in need of review"
+    exit 1
+fi
