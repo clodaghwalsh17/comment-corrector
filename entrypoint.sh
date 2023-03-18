d1cd2ce633f6d48ae7b04f827947770b6631c570
Clodagh Walsh <clodaghwalsh17@gmail.com>
2023-03-18 20:25:50 +0000

    Pass file outside of image as input to comment corrector
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,14 @@
 #!/bin/sh -l
 
-output=$(cd / && python3 -m comment_corrector v1.py v2.py)
+output=$(cd / && python3 -m comment_corrector file_a.py file_b.py)
 
 if [ "$output" = "" ]; then
     echo "Comment Corrector identified no comments in need of review"
     exit 0
 else
     echo "::warning ::Comment Corrector identified comments in need of review"
-    echo "::group::Comments to Review"
     echo "::group::v2.py"
     echo "$output"
-    echo "::endgroup::"
     echo "::endgroup::"
     exit 1
 fi
