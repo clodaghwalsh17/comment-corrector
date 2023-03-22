cd07d78bb66c3b7e17c9c8b7a3b4b2e96b04723d
Clodagh Walsh <clodaghwalsh17@gmail.com>
2023-03-22 17:17:14 +0000

    WIP: Using commit sha to get previous version of the file
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-output=$(cd / && python3 -m comment_corrector /github/workspace/file_a.py /github/workspace/file_b.py -w $1)
+output=$(cd / && python3 -m comment_corrector /github/workspace/file_a.py /github/workspace/ea55e3b5ce8794add6ba14d6933da320dd860dae/file_b.py -w $1)
 
 if [ "$output" = "" ]; then
     echo "Comment Corrector identified no comments in need of review"
