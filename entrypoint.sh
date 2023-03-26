f6bd64be6991f29683c99516504d0e5591ba3387
Clodagh Walsh <clodaghwalsh17@gmail.com>
2023-03-26 11:59:15 +0100

    Fix filepath to comment_corrector
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ tmp_file="/tmp/${strarr[0]}_prev.${strarr[1]}"
 touch "$tmp_file"
 echo $file_content >> "$tmp_file"
 
-output=$(python3 -m /comment_corrector "$file" "$tmp_file" -w $1)
+output=$(cd / && python3 -m comment_corrector "$tmp_file" "$file" -w $1)
 
 if [ "$output" = "" ]; then
     echo "Comment Corrector identified no comments in need of review"
