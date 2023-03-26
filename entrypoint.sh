db9ec1346b8aed72b2ab41611ac4ffd6deb84dd9
Clodagh Walsh <clodaghwalsh17@gmail.com>
2023-03-26 16:37:08 +0100

    Improve logic for saving temporary file
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,28 +3,30 @@ files=$(cd / && python3 retrieve_files.py)
 
 for file in ${files[@]}; 
 do
-    echo "$file"
-    current_file="/github/workspace/$file"
+    workspace_file="/github/workspace/$file"
 
     sha=$(cd /github/workspace && git log --skip=1 --max-count=1 --pretty=format:%H "$file")
     # TODO check that sha is not empty
     file_content=$(git show $sha:"$file")
-    # TODO account for case where file inside a folder
-    tmp_file="/tmp/$file" 
+
+    tmp_filename=$(awk -F/ '{print $NF}' <<< "$file")
+    tmp_file="/tmp/$tmp_filename" 
     touch "$tmp_file"
     echo "$file_content" >> "$tmp_file"
 
-    output=$(cd / && python3 -m comment_corrector "$tmp_file" "$current_file" -w "$1")
+    output=$(cd / && python3 -m comment_corrector "$tmp_file" "$workspace_file" -w "$1")
 
     # TODO check if exit code of comment corrector is not zero
     if [ "$output" = "" ]; then
-        echo "Comment Corrector identified no comments in need of review"
+        echo "Comment Corrector identified no comments in need of review in the file "$file""
     else
         echo "::warning ::Comment Corrector identified comments in need of review"
         echo "::group::"$file""
         echo "$output"
         echo "::endgroup::"
     fi
+
+    rm "$tmp_file"
 done
 
 exit 0
