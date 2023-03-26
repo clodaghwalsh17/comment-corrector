4c7efd946e7780117fb06cf50fd8c6b5cab60bd1
Clodagh Walsh <clodaghwalsh17@gmail.com>
2023-03-26 12:50:06 +0100

    Edit filepath
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,18 @@
 #!/bin/bash -l
-cd /github/workspace
-
 IFS='.'
 
 file="file_a.py"
 
-sha=$(git log --skip=1 --max-count=1 --pretty=format:%H "$file")
+sha=$(cd /github/workspace && git log --skip=1 --max-count=1 --pretty=format:%H "$file")
 file_content=$(git show $sha:"$file")
 read -a strarr <<< "$file"
 tmp_file="/tmp/${strarr[0]}_prev.${strarr[1]}"
 touch "$tmp_file"
 echo $file_content >> "$tmp_file"
 
-cat "$tmp_file"
+$current_file = "/github/workspace/$file"
 
-output=$(cd / && python3 -m comment_corrector "$tmp_file" "$file" -w "$1")
+output=$(cd / && python3 -m comment_corrector "$tmp_file" "$current_file" -w "$1")
 
 if [ "$output" = "" ]; then
     echo "Comment Corrector identified no comments in need of review"
