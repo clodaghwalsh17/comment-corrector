a134cb6d69ccb4f8cdaee45ddfdbbb8b98b1f0f5
Clodagh Walsh <clodaghwalsh17@gmail.com>
2023-03-26 13:25:22 +0100

    Fix save of temporary file
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,7 @@ file="file_a.py"
 
 sha=$(cd /github/workspace && git log --skip=1 --max-count=1 --pretty=format:%H "$file")
 file_content=$(git show $sha:"$file")
-read -a strarr <<< "$file"
-tmp_file="/tmp/${strarr[0]}_prev.${strarr[1]}"
+tmp_file="/tmp/$file"
 touch "$tmp_file"
 echo "$file_content" >> "$tmp_file"
 
