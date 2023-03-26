057b1f0a0417c0c1e5f68d82b828428b7cf6e3dc
Clodagh Walsh <clodaghwalsh17@gmail.com>
2023-03-26 15:52:39 +0100

    Retrieve list of files within repo that should be examined
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,29 @@
 #!/bin/bash -l
 IFS='.'
 
-file="file_a.py"
+files=$(cd / && python3 retrieve_files.py)
 
-sha=$(cd /github/workspace && git log --skip=1 --max-count=1 --pretty=format:%H "$file")
-file_content=$(git show $sha:"$file")
-tmp_file="/tmp/$file"
-touch "$tmp_file"
-echo "$file_content" >> "$tmp_file"
+for file in ${files[@]}; 
+do
+    echo "$file"
+    sha=$(cd /github/workspace && git log --skip=1 --max-count=1 --pretty=format:%H "$file")
+    # TODO check that sha is not empty
+    file_content=$(git show $sha:"$file")
+    tmp_file="/tmp/$file"
+    touch "$tmp_file"
+    echo "$file_content" >> "$tmp_file"
 
-current_file="/github/workspace/$file"
+    output=$(cd / && python3 -m comment_corrector "$tmp_file" "$file" -w "$1")
 
-output=$(cd / && python3 -m comment_corrector "$tmp_file" "$current_file" -w "$1")
-
-if [ "$output" = "" ]; then
-    echo "Comment Corrector identified no comments in need of review"
-else
-    echo "::warning ::Comment Corrector identified comments in need of review"
-    echo "::group::"$file""
-    echo "$output"
-    echo "::endgroup::"
-fi
+    # TODO check if exit code of comment corrector is not zero
+    if [ "$output" = "" ]; then
+        echo "Comment Corrector identified no comments in need of review"
+    else
+        echo "::warning ::Comment Corrector identified comments in need of review"
+        echo "::group::"$file""
+        echo "$output"
+        echo "::endgroup::"
+    fi
+done
 
 exit 0
