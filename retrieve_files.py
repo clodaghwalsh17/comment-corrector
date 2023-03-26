ea31b5ef4d71a053333041ad888b7d584c20ce0c
Clodagh Walsh <clodaghwalsh17@gmail.com>
2023-03-26 16:04:57 +0100

    Remove /github/workspace prefix
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- retrieve_files.py
+++ retrieve_files.py
@@ -6,4 +6,4 @@ GITHUB_WORKSPACE = '/github/workspace/'
 for (dirpath, _, filenames) in os.walk(GITHUB_WORKSPACE):
     for filename in filenames: 
         if Utils.get_file_extension(filename) == ".py":
-            print(os.path.join(dirpath, filename))
+            print(os.path.join(dirpath, filename).removeprefix(GITHUB_WORKSPACE))
