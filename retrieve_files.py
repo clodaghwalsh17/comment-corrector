61a18ffd6112f5fd2f8413a0495ea4c4b10cd180
Clodagh Walsh <clodaghwalsh17@gmail.com>
2023-03-27 18:17:01 +0100

    Temporary removal of .py file check for testing purposes
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- retrieve_files.py
+++ retrieve_files.py
@@ -5,5 +5,4 @@ GITHUB_WORKSPACE = '/github/workspace/'
 
 for (dirpath, _, filenames) in os.walk(GITHUB_WORKSPACE):
     for filename in filenames: 
-        if Utils.get_file_extension(filename) == ".py":
-            print(os.path.join(dirpath, filename).removeprefix(GITHUB_WORKSPACE))
+        print(os.path.join(dirpath, filename).removeprefix(GITHUB_WORKSPACE))
