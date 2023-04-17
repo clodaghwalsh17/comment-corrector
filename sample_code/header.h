ce67f9a458cf56593d21b7657adb3aad71e0ebad
Clodagh Walsh <clodaghwalsh17@gmail.com>
2023-04-17 16:35:35 +0100

    Update README and delete sample_code folder
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- sample_code/header.h
+++
@@ -1,6 +0,0 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
-// Sample header file
-
-#endif
--- sample_code/helloworld.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-// C program
-int main() {
-   printf("Hello, World!");
-   return 0;
-}
